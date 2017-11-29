0703c884ddbb0db920800cb8cd181a98854a7a5d
silvee <silvee.max@gmail.com>
2017-11-29 17:22:17 +0200

    Added basic activity with viewpager which will be used instead of CrimeActivity

---
+++ app/src/main/java/com/example/silvee/criminal/CrimePagerActivity.java
@@ -0,0 +1,43 @@
+package com.example.silvee.criminal;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        viewPager = findViewById(R.id.crime_view_pager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+    }
+
+
+
+}
