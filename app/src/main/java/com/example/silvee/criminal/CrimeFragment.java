74be383fabc1fee05fa1998aaa168f93ecf99755
silvee <silvee.max@gmail.com>
2017-11-30 14:43:42 +0200

    Added buttons Previous and Next to slide through crimes in CrimePagerActivity. They are disabled if you are at the start or at the end of the list of crimes

--- app/src/main/java/com/example/silvee/criminal/CrimeFragment.java
+++ app/src/main/java/com/example/silvee/criminal/CrimeFragment.java
@@ -41,7 +41,7 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField = v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
@@ -61,11 +61,11 @@ public class CrimeFragment extends Fragment {
             }
         });
 
-        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton = v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
 
-        mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckbox = v.findViewById(R.id.crime_solved);
         mSolvedCheckbox.setChecked(mCrime.isSolved());
         mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
--- app/src/main/java/com/example/silvee/criminal/CrimePagerActivity.java
+++ app/src/main/java/com/example/silvee/criminal/CrimePagerActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +20,8 @@ public class CrimePagerActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private List<Crime> mCrimes;
+    private Button buttonNext;
+    private Button buttonPrevious;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,33 @@ public class CrimePagerActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
 
+        // Buttons Next and Previous
+        buttonNext = findViewById((R.id.next_crime_button));
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (viewPager.getCurrentItem() == viewPager.getAdapter().getCount() - 2) {
+                    buttonNext.setEnabled(false);
+                } else if (viewPager.getCurrentItem() == 0) {
+                    buttonPrevious.setEnabled(true);
+                }
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+            }
+        });
+        buttonPrevious = findViewById(R.id.prev_crime_button);
+        buttonPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (viewPager.getCurrentItem() == 1) {
+                    buttonPrevious.setEnabled(false);
+                } else if (viewPager.getCurrentItem() == viewPager.getAdapter().getCount() - 1) {
+                    buttonNext.setEnabled(true);
+                }
+                viewPager.setCurrentItem(viewPager.getCurrentItem() - 1,  true );
+            }
+        });
+
+        // Setting Adapter for ViewPager
         FragmentManager fragmentManager = getSupportFragmentManager();
         viewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
@@ -41,10 +72,14 @@ public class CrimePagerActivity extends AppCompatActivity {
             }
         });
 
+
+        // Open Crime that was clicked, not the first
         UUID crimeID = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         for (Crime crime : mCrimes) {
             if (crime.getId().equals(crimeID)) {
                 viewPager.setCurrentItem(mCrimes.indexOf(crime));
+                if (viewPager.getCurrentItem() == viewPager.getAdapter().getCount() - 1) buttonNext.setEnabled(false);
+                if (viewPager.getCurrentItem() == 0) buttonPrevious.setEnabled(false);
                 break;
             }
         }
